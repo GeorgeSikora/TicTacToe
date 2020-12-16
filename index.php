d22e396d8ba5c2cecab9f56bfc28c83d5fd21706
GeorgeSikora <jirisikoracz@email.cz>
2020-12-16 20:52:19 +0100

    new styles

--- index.php
+++ index.php
@@ -2,18 +2,22 @@
 <head>
 
     <link rel="stylesheet" href="style/main.css">
+    <link rel="stylesheet" href="style/time.css">
+    <link rel="stylesheet" href="style/message.css">
+
     <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.1.9/p5.min.js"></script>
 
     <script src="main.js"></script>
 
 </head>
 <body>
-
+    <!-- TOP CONTENT -->
     <p id="time" class="time"></p>
     <div class="timeout">
         <p id="timeout">timeout: <span id="timeout-value"></span>s</p>
     </div>
 
+    <!-- BOTTOM CONTENT -->
     <div id="bottom" class="bottom">
         <p id="message" class="message"></p>
     </div>
