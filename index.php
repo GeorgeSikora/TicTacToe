3013615e68eadbe4366a259e6c39c0d678d8837a
GeorgeSikora <jirisikoracz@email.cz>
2020-12-17 21:55:20 +0100

    importing new files

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <html>
 <head>
 
+    <meta name="viewport" content= "width=device-width, user-scalable=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <link rel="stylesheet" href="style/main.css">
     <link rel="stylesheet" href="style/time.css">
     <link rel="stylesheet" href="style/message.css">
@@ -8,6 +11,12 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.1.9/p5.min.js"></script>
 
     <script src="main.js"></script>
+    <script src="timer.js"></script>
+    <script src="field.js"></script>
+    <script src="cell.js"></script>
+    <script src="functions.js"></script>
+    <script src="keyMouse.js"></script>
+    <script src="bot.js"></script>
 
 </head>
 <body>
